9d672e6192cc95174ef1afcbdd8073d599f6d971
salma-c <92518434+salma-c@users.noreply.github.com>
2022-01-14 15:32:16 +0100

    Add update1

--- component_nav-bar_desktop.js
+++ component_nav-bar_desktop.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
  
 
@@ -7,10 +7,8 @@
 
 const tem = document.createElement("tem");
 tem.innerHTML = `
-=======
-const templat = document.createElement("template");
-templat.innerHTML = `
->>>>>>> 2d91f8e46b07cb5a912d0a262550244e33497c27
+
+
 <header>
 <div class="nav-bar">
     <img src="logo.png" class="logo">
@@ -32,11 +30,11 @@ templat.innerHTML = `
     
     class navbar_desktop extends HTMLElement{
         connectedCallback(){
-<<<<<<< HEAD
+
             this.innerHTML = tem.innerHTML;
-=======
+
             this.innerHTML = templat.innerHTML;
->>>>>>> 2d91f8e46b07cb5a912d0a262550244e33497c27
+
         }
     }
     customElements.define("navbar-com",navbar_desktop);
--- responsive navbar.js
+++
@@ -1,7 +0,0 @@
-const menuu = document.querySelector(".menu-toggle");
-                 const nav = document.querySelector(".nav-bar2");
-                 const handleClick= function(){
-                 nav.classList.toggle("nav-bar2-open")
-                }
-menuu.addEventListener("click", handleClick)
-nav.addEventListener("click", handleClick)
